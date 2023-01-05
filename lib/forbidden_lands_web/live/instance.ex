6f67d8d97945f7ed6a4a82981e935947bbe11044
abdelaz3r <gil.clavien@gmail.com>
2023-01-05 22:56:38 +0100

    Minor visual improvements

--- lib/forbidden_lands_web/live/instance.ex
+++ lib/forbidden_lands_web/live/instance.ex
@@ -17,11 +17,13 @@ defmodule ForbiddenLandsWeb.Live.Instance do
   def mount(_params, _session, socket) do
     now = Calendar.from_quarters(@current_quarter)
     quarter_shift = now.count.quarters - rem(now.count.quarters - 1, 4)
+    messages = Enum.map(1..20, fn i -> Calendar.add(now, i * 32 + Enum.random(1..3), :quarter) end)
 
     socket =
       socket
       |> assign(now: now)
       |> assign(quarter_shift: quarter_shift)
+      |> assign(messages: messages)
 
     {:ok, socket}
   end
@@ -49,7 +51,26 @@ defmodule ForbiddenLandsWeb.Live.Instance do
           class="flex-none z-10 border-b border-slate-900 shadow-2xl shadow-black/50"
         />
 
-        <div class="grow overflow-y-auto flex flex-col gap-5 p-5">
+        <div class="grow overflow-y-auto flex flex-col gap-4 p-4 font-title text-slate-100">
+          <section :for={message <- @messages} class="pb-4 border-b border-slate-900/50">
+            <header class="flex justify-between">
+              <h2 class="font-bold">
+                Title
+              </h2>
+              <div>
+                <%= message.month.day %>
+                <%= message.month.name %>
+                <span class="opacity-50">
+                  <%= message.year.number %>,
+                  <span class="opacity-50">
+                    <%= message.quarter.name %>
+                  </span>
+                </span>
+              </div>
+            </header>
+            <p class="text-sm">Un événement au bol...</p>
+          </section>
+
           <div class="flex flex-wrap gap-2">
             <.button
               :for={amount <- [1, 4, 28, 180, 1460, -1, -4, -28, -180, -1460]}
@@ -61,15 +82,13 @@ defmodule ForbiddenLandsWeb.Live.Instance do
               <%= amount %> Quarter
             </.button>
           </div>
-
-          <div class="text-slate-400">
-            <%= inspect(@now) %>
-          </div>
         </div>
 
-        <div class="flex-none h-40 font-title text-slate-100 border-t border-slate-900 shadow-2xl shadow-black/50">
-          <div class="p-5">
-            Castle info
+        <div class="flex-none h-40 font-title text-slate-100 border-t border-slate-900 shadow-2xl shadow-black/50 bg-gradient-to-l from-slate-800 to-slate-900">
+          <div class="p-4">
+            <h1 class="flex gap-4 text-lg font-bold">
+              <Heroicons.bookmark class="w-6" /> Weatherstone
+            </h1>
           </div>
         </div>
       </div>
