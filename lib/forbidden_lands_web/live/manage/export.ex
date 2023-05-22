0f0f1ce54549b3ecc275737c50f325f7c71fe187
abdelaz3r <gil.clavien@gmail.com>
2023-05-22 21:24:12 +0200

    Create export panel in manage view

---
+++ lib/forbidden_lands_web/live/manage/export.ex
@@ -0,0 +1,61 @@
+defmodule ForbiddenLandsWeb.Live.Manage.Export do
+  @moduledoc """
+  Export of an instance.
+  """
+
+  use ForbiddenLandsWeb, :live_component
+
+  alias ForbiddenLands.Export
+
+  @impl Phoenix.LiveComponent
+  def mount(socket) do
+    {:ok, assign(socket, export?: false)}
+  end
+
+  @impl Phoenix.LiveComponent
+  def update(assigns, socket) do
+    {:ok, assign(socket, instance: assigns.instance)}
+  end
+
+  @impl Phoenix.LiveComponent
+  def render(assigns) do
+    ~H"""
+    <div class="p-6">
+      <section>
+        <div class="flex justify-between pb-3">
+          <h2 class="text-xl font-bold">
+            <%= dgettext("manage", "Exporter") %>
+          </h2>
+
+          <.button :if={not @export?} phx-click="export" phx-target={@myself}>
+            <%= dgettext("manage", "Exporter l'instance") %>
+          </.button>
+          <.button
+            :if={@export?}
+            id="clipboard"
+            data-value-success={dgettext("manage", "Copie réussie")}
+            data-value-error={dgettext("manage", "Copie échouée")}
+            data-to="#clipboard-target"
+            phx-hook="copy-to-clipboard"
+          >
+            <%= dgettext("manage", "Copier dans le presse-papier") %>
+          </.button>
+        </div>
+
+        <textarea :if={@export?} id="clipboard-target" class="w-full h-64" readonly={true}><%= export(@instance) %></textarea>
+      </section>
+    </div>
+    """
+  end
+
+  @impl Phoenix.LiveComponent
+  def handle_event("export", _params, socket) do
+    {:noreply, assign(socket, export?: true)}
+  end
+
+  defp export(instance) do
+    instance
+    |> Export.export()
+    |> inspect(pretty: true, limit: :infinity)
+  end
+end
