a8a69b4df796562fe8a9c58e22c2260d30c4ee1c
abdelaz3r <gil.clavien@gmail.com>
2023-05-27 18:49:02 +0200

    Replace heroicons with lucide

--- lib/forbidden_lands_web/components/core_components.ex
+++ lib/forbidden_lands_web/components/core_components.ex
@@ -5,9 +5,6 @@ defmodule ForbiddenLandsWeb.CoreComponents do
   The components in this module use Tailwind CSS, a utility-first CSS framework.
   See the [Tailwind CSS documentation](https://tailwindcss.com) to learn how to
   customize the generated components in this module.
-
-  Icons are provided by [heroicons](https://heroicons.com), using the
-  [heroicons_elixir](https://github.com/mveytsman/heroicons_elixir) project.
   """
   use Phoenix.Component
 
