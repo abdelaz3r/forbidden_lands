d551c7201d04da1b1af6260c7453ada217f18574
abdelaz3r <gil.clavien@gmail.com>
2023-07-14 16:35:20 +0200

    Update deps; remove live_dashboard from deps

--- lib/forbidden_lands_web/endpoint.ex
+++ lib/forbidden_lands_web/endpoint.ex
@@ -33,11 +33,6 @@ defmodule ForbiddenLandsWeb.Endpoint do
     plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :forbidden_lands)
   end
 
-  plug(Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
-  )
-
   plug(Plug.RequestId)
   plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
--- mix.exs
+++ mix.exs
@@ -32,15 +32,14 @@ defmodule ForbiddenLands.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.3"},
+      {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_live_view, "~> 0.19.0"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
