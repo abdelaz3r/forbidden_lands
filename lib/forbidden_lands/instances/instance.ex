401fa281b12151893a2fe151fd78aaf9b56a1b6c
abdelaz3r <gil.clavien@gmail.com>
2023-07-19 14:09:05 +0200

    Add credential infos to instance struct

--- lib/forbidden_lands/instances/instance.ex
+++ lib/forbidden_lands/instances/instance.ex
@@ -27,6 +27,9 @@ defmodule ForbiddenLands.Instances.Instance do
   @type t() :: %Instance{
           id: non_neg_integer() | nil,
           name: String.t() | nil,
+          username: String.t() | nil,
+          password: String.t() | nil,
+          hashed_password: String.t() | nil,
           prepend_name: String.t() | nil,
           append_name: String.t() | nil,
           human_date: String.t() | nil,
@@ -35,10 +38,15 @@ defmodule ForbiddenLands.Instances.Instance do
           mood: String.t() | nil,
           description: String.t() | nil,
           introduction: String.t() | nil,
-          stronghold: Stronghold.t() | nil
+          stronghold: Stronghold.t() | nil,
+          resource_rules: [ResourceRule.t()] | nil,
+          events: [Event.t()] | nil
         }
   schema("instances") do
     field(:name, :string)
+    field(:username, :string)
+    field(:password, :string, virtual: true)
+    field(:hashed_password, :string, redact: true)
     field(:prepend_name, :string)
     field(:append_name, :string)
     field(:human_date, :string, virtual: true)
@@ -57,29 +65,52 @@ defmodule ForbiddenLands.Instances.Instance do
   @spec create(Instance.t(), map()) :: Ecto.Changeset.t()
   def create(instance, params \\ %{}) do
     instance
-    |> cast(params, [:name, :human_date])
-    |> validate_required([:name, :human_date])
+    |> cast(params, [:name, :username, :password, :human_date])
+    |> validate_required([:name, :username, :password, :human_date])
     |> ForbiddenLands.Validation.validate_date(:human_date)
     |> put_dates()
+    |> maybe_hash_password()
   end
 
   @spec create_from_export(Instance.t(), map()) :: Ecto.Changeset.t()
   def create_from_export(instance, params \\ %{}) do
     instance
-    |> cast(params, [:name, :initial_date, :current_date, :prepend_name, :append_name, :description, :introduction])
-    |> validate_required([:name, :initial_date, :current_date])
+    |> cast(params, [
+      :name,
+      :username,
+      :password,
+      :initial_date,
+      :current_date,
+      :prepend_name,
+      :append_name,
+      :description,
+      :introduction
+    ])
+    |> validate_required([:name, :username, :password, :initial_date, :current_date])
     |> cast_embed(:stronghold, with: &Stronghold.changeset/2)
     |> cast_embed(:resource_rules, with: &ResourceRule.create/2)
     |> cast_assoc(:events, with: &Event.create_from_export/2)
+    |> maybe_hash_password()
   end
 
   @spec update(Instance.t(), map(), list()) :: Ecto.Changeset.t()
   def update(instance, params \\ %{}, resource_rules \\ []) do
     instance
-    |> cast(params, [:name, :current_date, :mood, :prepend_name, :append_name, :description, :introduction])
+    |> cast(params, [
+      :name,
+      :username,
+      :password,
+      :current_date,
+      :mood,
+      :prepend_name,
+      :append_name,
+      :description,
+      :introduction
+    ])
     |> validate_required([:name, :current_date, :mood])
     |> cast_embed(:stronghold, with: &Stronghold.changeset/2)
     |> put_embed(:resource_rules, resource_rules)
+    |> maybe_hash_password()
   end
 
   defp put_dates(instance) do
@@ -91,6 +122,19 @@ defmodule ForbiddenLands.Instances.Instance do
       |> put_change(:current_date, calendar.count.quarters)
     else
       instance
+    end
+  end
+
+  defp maybe_hash_password(changeset) do
+    password = get_change(changeset, :password)
+
+    if password do
+      changeset
+      |> validate_length(:password, max: 72, count: :bytes)
+      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
+      |> delete_change(:password)
+    else
+      changeset
     end
   end
 end
