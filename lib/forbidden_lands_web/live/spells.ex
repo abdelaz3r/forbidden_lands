c36ebd51ee4cee5df1068ee2c5bf1250696c0e06
abdelaz3r <gil.clavien@gmail.com>
2023-11-24 15:46:51 +0100

    Create spells struct

--- lib/forbidden_lands_web/live/spells.ex
+++ lib/forbidden_lands_web/live/spells.ex
@@ -7,6 +7,8 @@ defmodule ForbiddenLandsWeb.Live.Spells do
 
   import ForbiddenLandsWeb.Components.Navbar
 
+  alias ForbiddenLands.Items.Spell
+
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     # Load spells from JSON file
@@ -28,19 +30,22 @@ defmodule ForbiddenLandsWeb.Live.Spells do
       spells_json
       |> Enum.reduce({MapSet.new(), MapSet.new(), MapSet.new()}, fn spell, {types, durations, ranges} ->
         {
-          MapSet.put(types, {type_locale(spell["type"]), spell["type"]}),
-          MapSet.put(durations, {duration_locale(spell["duration"]), spell["duration"]}),
-          MapSet.put(ranges, {range_locale(spell["range"]), spell["range"]})
+          MapSet.put(types, {Spell.types(spell["type"]), spell["type"]}),
+          MapSet.put(durations, {Spell.durations(spell["duration"]), spell["duration"]}),
+          MapSet.put(ranges, {Spell.ranges(spell["range"]), spell["range"]})
         }
       end)
 
     # Set ranks
     ranks = Enum.map(1..3, fn i -> {dgettext("spells", "Rank %{level}", level: i), i} end)
 
+    # Transform spells JSON into a list of spells
+    spells = Enum.map(spells_json, fn map -> Spell.from_json(map) end)
+
     socket =
       socket
       |> assign(page_title: dgettext("app", "Spells list"))
-      |> assign(spells_json: spells_json)
+      |> assign(spells: spells)
       |> assign(types: [{dgettext("spells", "All types"), "all"} | types |> MapSet.to_list()])
       |> assign(durations: [{dgettext("spells", "All durations"), "all"} | durations |> MapSet.to_list()])
       |> assign(ranges: [{dgettext("spells", "All ranges"), "all"} | ranges |> MapSet.to_list()])
@@ -52,7 +57,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
 
   @impl Phoenix.LiveView
   def handle_params(_params, _uri, socket) do
-    {:noreply, filter_json(socket)}
+    {:noreply, filter_spells(socket)}
   end
 
   @impl Phoenix.LiveView
@@ -83,7 +88,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
           class="relative text-left overflow-hidden m-2 w-[290px] h-[403px] inline-block align-top text-sm border-8 rounded bg-white"
           style={"border-color: rgba(#{spell.style}, .5);"}
         >
-          <div class="absolute inset-0" style={spell.background}></div>
+          <div class="absolute inset-0" style={background(spell.type, spell.style)}></div>
           <div class="absolute top-4 left-0 h-8 border-4 border-transparent border-l-white"></div>
           <div class="absolute top-4 left-0 h-8 border-4 border-transparent" style={"border-left-color: rgba(#{spell.style}, .5);"}>
           </div>
@@ -128,11 +133,11 @@ defmodule ForbiddenLandsWeb.Live.Spells do
               <div class="flex justify-between pb-2">
                 <div class="flex items-baseline gap-2">
                   <span>
-                    <%= spell.type_locale %>
+                    <%= Spell.types(spell.type) %>
                   </span>
                   <span class="flex gap-1">
                     <span
-                      :for={width <- spell.pattern}
+                      :for={width <- Spell.patterns(spell.type)}
                       class="h-1 rounded-md"
                       style={"background: rgba(#{spell.style}, 1); width: #{width * 4}px;"}
                     >
@@ -151,8 +156,8 @@ defmodule ForbiddenLandsWeb.Live.Spells do
 
               <div class="text-base border-t border-b" style={"border-color: rgba(#{spell.style}, .25);"}>
                 <div class="flex justify-between pt-2">
-                  <div><%= spell.range_locale %></div>
-                  <div><%= spell.duration_locale %></div>
+                  <div><%= Spell.ranges(spell.type) %></div>
+                  <div><%= Spell.durations(spell.type) %></div>
                 </div>
 
                 <div class="flex items-center gap-2 pb-2">
@@ -195,7 +200,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
       |> assign(filters_duration: changeset["duration"])
       |> assign(filters_range: changeset["range"])
       |> assign(filters_rank: String.to_integer(changeset["rank"]))
-      |> filter_json()
+      |> filter_spells()
 
     {:noreply, socket}
   end
@@ -209,7 +214,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
     |> Ecto.Changeset.apply_changes()
   end
 
-  defp filter_json(socket) do
+  defp filter_spells(socket) do
     filters_text = Map.get(socket.assigns, :filters_text, "")
     filters_type = Map.get(socket.assigns, :filters_type, "all")
     filters_duration = Map.get(socket.assigns, :filters_duration, "all")
@@ -217,77 +222,32 @@ defmodule ForbiddenLandsWeb.Live.Spells do
     filters_rank = Map.get(socket.assigns, :filters_rank, 0)
 
     spells =
-      socket.assigns.spells_json
+      socket.assigns.spells
       |> Enum.filter(fn spell ->
-        String.match?(spell["name"], ~r/#{filters_text}/i) and
-          (filters_type == "all" or spell["type"] == filters_type) and
-          (filters_duration == "all" or spell["duration"] == filters_duration) and
-          (filters_range == "all" or spell["range"] == filters_range) and
-          (filters_rank == 0 or spell["rank"] == filters_rank)
+        String.match?(spell.name, ~r/#{filters_text}/i) and
+          (filters_type == "all" or spell.type == filters_type) and
+          (filters_duration == "all" or spell.duration == filters_duration) and
+          (filters_range == "all" or spell.range == filters_range) and
+          (filters_rank == 0 or spell.rank == filters_rank)
       end)
-      |> Enum.sort_by(fn spell -> spell["rank"] end)
-      |> Enum.sort_by(fn spell -> spell["type"] end)
-      |> Enum.map(fn spell ->
-        %{
-          type: spell["type"],
-          type_locale: type_locale(spell["type"]),
-          rank: spell["rank"],
-          range: spell["range"],
-          range_locale: range_locale(spell["range"]),
-          name: spell["name"],
-          name_length: String.length(spell["name"]),
-          is_official: spell["is_official"],
-          is_ritual: spell["is_ritual"],
-          is_power_word: spell["is_power_word"],
-          do_consume_ingredient: spell["do_consume_ingredient"],
-          ingredient: spell["ingredient"],
-          duration: spell["duration"],
-          duration_locale: duration_locale(spell["duration"]),
-          desc_header: spell["description"]["header"],
-          desc_summary: Enum.join(spell["description"]["summary"], " "),
-          desc_length:
-            String.length(spell["description"]["header"] <> Enum.join(spell["description"]["summary"], " ")),
-          style: color_by_type(spell["type"]),
-          background: background_by_type(spell["type"], color_by_type(spell["type"])),
-          pattern: pattern_by_type(spell["type"])
-        }
-      end)
+      |> Enum.sort_by(fn spell -> spell.rank end)
+      |> Enum.sort_by(fn spell -> spell.type end)
 
     assign(socket, spells: spells)
   end
 
-  defp color_by_type("general"), do: "114, 114, 114"
-  defp color_by_type("healing"), do: "83, 61, 151"
-  defp color_by_type("shapeshifting"), do: "91, 153, 51"
-  defp color_by_type("awareness"), do: "41, 169, 161"
-  defp color_by_type("symbolism"), do: "53, 98, 180"
-  defp color_by_type("stone_song"), do: "173, 109, 90"
-  defp color_by_type("blood_magic"), do: "195, 35, 85"
-  defp color_by_type("death_magic"), do: "137, 112, 140"
-  defp color_by_type(_), do: "0, 0, 0"
-
-  defp pattern_by_type("general"), do: [3, 1]
-  defp pattern_by_type("healing"), do: [1, 1, 4]
-  defp pattern_by_type("shapeshifting"), do: [2, 2, 2]
-  defp pattern_by_type("awareness"), do: [4, 2, 2]
-  defp pattern_by_type("symbolism"), do: [1, 2, 1, 2, 1]
-  defp pattern_by_type("stone_song"), do: [7, 1]
-  defp pattern_by_type("blood_magic"), do: [1, 1, 1, 1]
-  defp pattern_by_type("death_magic"), do: [2, 5, 1]
-  defp pattern_by_type(_), do: [5]
-
-  defp background_by_type("general", color) do
+  defp background("general", color) do
     "background: linear-gradient(to bottom left, rgba(#{color}, .1), rgba(#{color}, .0));"
   end
 
-  defp background_by_type("healing", color) do
+  defp background("healing", color) do
     "background-image:
        repeating-radial-gradient(circle at 0 0, transparent 0, rgba(#{color}, .08) 10px),
        repeating-linear-gradient(rgba(#{color}, .01), rgba(#{color}, .02));
      background-color: rgba(#{color}, .05);"
   end
 
-  defp background_by_type("shapeshifting", color) do
+  defp background("shapeshifting", color) do
     "--c: rgba(#{color}, .1);
      background:
        radial-gradient(circle at bottom left, var(--c) 35%, transparent 36%),
@@ -297,7 +257,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: -5px 0;"
   end
 
-  defp background_by_type("awareness", color) do
+  defp background("awareness", color) do
     "--c: rgba(#{color}, .1);
      background: radial-gradient(
        circle at center, var(--c), var(--c) 10%, transparent 10%, transparent 20%, var(--c) 20%, var(--c) 30%,
@@ -307,7 +267,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: 2px -10px;"
   end
 
-  defp background_by_type("symbolism", color) do
+  defp background("symbolism", color) do
     "--c: rgba(#{color}, .1);
      background:
        linear-gradient(45deg, transparent 34%, var(--c) 35%, var(--c) 40%, transparent 41%, transparent 59%, var(--c)  60%, var(--c) 65%, transparent 66%),
@@ -321,7 +281,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: -3px -3px;"
   end
 
-  defp background_by_type("stone_song", color) do
+  defp background("stone_song", color) do
     "--c: rgba(#{color}, .25);
      background:
        radial-gradient(circle, var(--c) 10%, transparent 11%),
@@ -333,7 +293,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: 0 -3px;"
   end
 
-  defp background_by_type("blood_magic", color) do
+  defp background("blood_magic", color) do
     "--c: rgba(#{color}, .18);
      background:
        linear-gradient(25deg, #ffffff 40%, transparent 41%, transparent 59%, #ffffff 60%),
@@ -343,7 +303,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: 3px 0;"
   end
 
-  defp background_by_type("death_magic", color) do
+  defp background("death_magic", color) do
     "--c: rgba(#{color}, .15);
      background:
        radial-gradient(circle, transparent 25%, #ffffff  26%),
@@ -353,37 +313,7 @@ defmodule ForbiddenLandsWeb.Live.Spells do
      background-position: -3px -2px;"
   end
 
-  defp background_by_type(_type, _color) do
+  defp background(_type, _color) do
     "background: white;"
   end
-
-  defp type_locale("general"), do: dgettext("spells", "Common Magic")
-  defp type_locale("healing"), do: dgettext("spells", "Healing")
-  defp type_locale("shapeshifting"), do: dgettext("spells", "Shapeshifting")
-  defp type_locale("awareness"), do: dgettext("spells", "Awareness")
-  defp type_locale("symbolism"), do: dgettext("spells", "Symbolism")
-  defp type_locale("stone_song"), do: dgettext("spells", "Stone Song")
-  defp type_locale("blood_magic"), do: dgettext("spells", "Blood Magic")
-  defp type_locale("death_magic"), do: dgettext("spells", "Death Magic")
-  defp type_locale(_), do: dgettext("spells", "Unknown Magic")
-
-  defp duration_locale("immediate"), do: dgettext("spells", "Immediate")
-  defp duration_locale("round"), do: dgettext("spells", "1 round")
-  defp duration_locale("round_per_level"), do: dgettext("spells", "1 round/lvl.")
-  defp duration_locale("turn"), do: dgettext("spells", "1 turn")
-  defp duration_locale("turn_per_level"), do: dgettext("spells", "1 turn/lvl.")
-  defp duration_locale("quarter"), do: dgettext("spells", "1 quarter")
-  defp duration_locale("quarter_per_level"), do: dgettext("spells", "1 quarter/lvl.")
-  defp duration_locale("varies"), do: dgettext("spells", "Varies")
-  defp duration_locale(_), do: dgettext("spells", "—")
-
-  defp range_locale("personal"), do: dgettext("spells", "Personal")
-  defp range_locale("arms_length"), do: dgettext("spells", "Arm's length")
-  defp range_locale("near"), do: dgettext("spells", "Near")
-  defp range_locale("short"), do: dgettext("spells", "Short")
-  defp range_locale("long"), do: dgettext("spells", "Long")
-  defp range_locale("distant"), do: dgettext("spells", "Distant")
-  defp range_locale("unlimited"), do: dgettext("spells", "Unlimited")
-  defp range_locale("varies"), do: dgettext("spells", "Varies")
-  defp range_locale(_), do: dgettext("spells", "—")
 end
